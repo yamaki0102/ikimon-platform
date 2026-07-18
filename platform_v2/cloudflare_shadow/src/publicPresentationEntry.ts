fea7459bed8a3c26062f918f4b1db5070c8aba46
yamaki0102 <39521701+yamaki0102@users.noreply.github.com>
2026-07-18 22:56:08 +0900

    feat(record): route photo uploads through atomic reassessment batch

--- platform_v2/cloudflare_shadow/src/publicPresentationEntry.ts
+++ platform_v2/cloudflare_shadow/src/publicPresentationEntry.ts
@@ -1,4 +1,5 @@
 import baseWorker from "./index";
+import { withAtomicPhotoReassessment } from "./atomicPhotoReassessmentEntry";
 import { enforceCameraFirstHomeCta } from "./cameraFirstHomeCta";
 import { polishPublicHomeUx } from "./publicHomeUxPolish";
 import { patchPublicHomePresentation } from "./publicPresentationPatch";
@@ -12,7 +13,11 @@ const delegatedWorker = baseWorker as DelegatedWorker;
 export default {
   ...delegatedWorker,
   async fetch(request: Request, env: unknown, ctx: unknown): Promise<Response> {
-    const response = await delegatedWorker.fetch.call(delegatedWorker, request, env, ctx);
+    const response = await withAtomicPhotoReassessment(
+      request,
+      env,
+      async (delegatedEnv) => delegatedWorker.fetch.call(delegatedWorker, request, delegatedEnv, ctx),
+    );
     const presented = await patchPublicHomePresentation(request, response);
     const cameraFirst = await enforceCameraFirstHomeCta(request, presented);
     return polishPublicHomeUx(request, cameraFirst);
